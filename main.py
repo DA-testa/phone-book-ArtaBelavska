9aca986640cd489faef17215c0cbd42d66a30fe3
ArtaBelavska <124885266+ArtaBelavska@users.noreply.github.com>
2023-03-25 21:59:25 +0000

    tests1

--- main.py
+++ main.py
@@ -3,42 +3,35 @@
 class Query:
     def __init__(self, query):
         self.type = query[0]
-        self.number = int(query[1])
+        self.number = int(query[1]) #saglaba otro elemetu numuru ka integer tipa
         if self.type == 'add':
-            self.name = query[2]
+            self.name = query[2] #saglaba treso elementu vardu 
 
 def read_queries():
-    n = int(input())
+    n = int(input()) # ievadit cik prasijumus bus jaizpilda
     return [Query(input().split()) for i in range(n)]
 
 def write_responses(result):
     print('\n'.join(result))
 
 def process_queries(queries):
-    result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    result = [] #glaba visus kontakus, iekavas jo saraksts
+    contacts = {} # glaba visus kontaktus kas nav izdzesti, iekavas jo vardnica
     for cur_query in queries:
         if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
+            # for contact in contacts:
+            #     if contact.number == cur_query.number:
+            if cur_query.number in contacts:
+                contacts[cur_query.number].name = cur_query.name # ja ir kontakts ar tādu pašu numuru, tad maina kontakta vardu
             else: # otherwise, just add it
-                contacts.append(cur_query)
+                contacts[cur_query.number]=cur_query
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+                if cur_query.number in contacts:
+                    del contacts[cur_query.number]
         else:
             response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            if cur_query.number in contacts:
+                response = contacts[cur_query.number].name
             result.append(response)
     return result
 
